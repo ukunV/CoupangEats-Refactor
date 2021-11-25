00710fdd1ce2e561f252ace01c6ad81a26a8bb6c
ukunV <showklee@naver.com>
2021-11-26 01:05:15 +0900

    refactor: crypto -> bcrypt

--- controllers/user_ctrl.ts
+++ controllers/user_ctrl.ts
@@ -1,50 +1,27 @@
-import * as crypto from 'crypto';
 import * as bcrypt from 'bcrypt';
 
-const createSalt = async function () {
-  const salt = await crypto.randomBytes(64).toString('base64');
-
-  return salt;
-};
-
-// const createHashedPassword = async function (password) {
-//   const salt = await createSalt();
-
-//   console.log(salt);
-
-//   const hashedPassword = await crypto
-//     .pbkdf2(password, salt, 9999, 64, "sha512")
-//     .toString("base64");
-
-//   return [salt, hashedPassword];
-// };
-
 export const createHashedPassword = async (plainPassword: string) => {
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) return err;
-    bcrypt.hash(plainPassword, salt, (err, hashedPassword) => {
-      if (err) return err;
-      return { hashedPassword, salt };
-    });
-  });
+  const salt = await bcrypt
+    .genSalt(10)
+    .then(salt => salt)
+    .catch(err => err.message);
+
+  const hashedPassword = await bcrypt
+    .hash(plainPassword, salt)
+    .then(hashedPassword => hashedPassword)
+    .catch(err => err.message);
+
+  return hashedPassword;
 };
 
-// export const createHashedPassword = async (plainPassword: string) =>
-//   new Promise(async (resolve, reject) => {
-//     const salt = await createSalt();
+export const checkPassword = async (
+  plainPassword: string,
+  hashedPassword: string
+) => {
+  const check = await bcrypt
+    .compare(plainPassword, hashedPassword)
+    .then(check => check)
+    .catch(err => err.message);
 
-//     crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
-//       if (err) reject(err);
-
-//       resolve({ hashedPassword: key.toString('base64'), salt });
-//     });
-//   });
-
-export const makePasswordHashed = (salt: string, plainPassword: string) =>
-  new Promise(async (resolve, reject) => {
-    crypto.pbkdf2(plainPassword, salt, 9999, 64, 'sha512', (err, key) => {
-      if (err) reject(err);
-
-      resolve(key.toString('base64'));
-    });
-  });
+  return check;
+};
