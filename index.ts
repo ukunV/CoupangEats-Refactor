f161272dd16c24c4de8090f17aa6a2dc3a9464c4
ukunV <showklee@naver.com>
2021-11-25 15:56:18 +0900

    refactor: index.ts

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { exp as express } from './config/express';
+import { logger } from './config/winston';
+
+let port;
+
+if (process.env.NODE_ENV === 'development') {
+  port = 3001;
+} else if (process.env.NODE_ENV === 'production') {
+  port = 3000;
+} else {
+  port = 9000;
+}
+
+express().listen(port);
+logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
