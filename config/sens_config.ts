38053dfc6e52a9900986a51d427fcb9094b6a27c
KUN UK LEE <showklee@naver.com>
2021-11-27 03:29:59 +0900

    Delete sens_config.ts

--- config/sens_config.ts
+++
@@ -1,6 +0,0 @@
-export const sensSecret = {
-  phoneNumber: '01054845421',
-  serviceId: 'ncp:sms:kr:263690737900:carrot-market',
-  secretKey: 'SmTeqPGh4Pbx3ZbkCFpUxyrlXiTcbegaSG8JeYaO',
-  accessKey: '74KP7ad46T87T5Teq88t',
-};
